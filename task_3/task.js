24ec2e080ca0e20e4706e3564923e9333fadea76
Deepakkumarnayak980 <123307079+Deepakkumarnayak980@users.noreply.github.com>
2024-06-12 06:56:03 +0530

    Add files via upload

---
+++ task_3/task.js
@@ -0,0 +1,22 @@
+let a =10;
+let b =20;
+let c =30;
+ 
+let sum = a+b+c;
+ 
+
+let result = `${a} + ${b} + ${c} = ${sum}`;
+//console.log(result);
+
+
+//task4
+
+let p =10;
+let q=20;
+let r =30;
+ 
+let sum1 = p*(b+c)-(b*c);
+ 
+
+let result1 = `${a} * (${b} + ${c}) - (${b} * ${c}) =  ${sum1}`;
+console.log(result1);
